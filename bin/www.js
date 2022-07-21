c8b1da01e2ea80ee3f353cf5819a19cab6b7b7e2
Morgan Scholz <mscholz.dev@gmail.com>
2022-07-21 15:41:41 +0200

    add vercel.json file

--- bin/www.js
+++
@@ -1,6 +0,0 @@
-const app = require("../app");
-
-const httpServer = require("./httpServer.js");
-
-httpServer.httpServer(app);
-console.debug("If you need to use Https server you must enable https and set path to key.pem and server.crt in config.properties");
